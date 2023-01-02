28b3500fb2730edb03ca2cee0e699e473c9ac205
SalomonMetre <kulosmetros088@gmail.com>
2023-01-03 00:45:00 +0300

    One lazy commit

--- Trials/convert_YT_links.py
+++ Trials/convert_YT_links.py
@@ -12,20 +12,26 @@ def get_y2mate_download_link(yt_link):
     return f"{protocol}://www.youtubepp.com/watch?v={video_id}"
 
 def open_download_pages(video_links):
-    for link in video_links:
-        initiate_download(get_y2mate_download_link(link))
-
-links=[]
-while(True):
-    user_choice=input('Would you like to enter a YouTube link? y/n\n')
-    if(user_choice=='y'):
-        link=input('Please enter the link : ')
-        links.append(link)
+    if len(video_links)!=0:
+        print('Get ready to download your YouTube videos ...')
+        for link in video_links:
+            initiate_download(get_y2mate_download_link(link))
     else:
-        print('Well noted.')
-        break
-print('Get ready to download your YouTube videos ...')
-open_download_pages(links)
+        print('No video links available. Could not initiate the download process...')
+
+def get_video_links():
+    links=[]
+    while(True):
+        user_choice=input('Would you like to enter a YouTube link? y/n\n')
+        if(user_choice=='y'):
+            link=input('Please enter the link : ')
+            links.append(link)
+        else:
+            print('Well noted.')
+            break
+    return links
+
+open_download_pages(get_video_links())
 
 
 
